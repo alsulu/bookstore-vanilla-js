46fc8032d32bc5dbcc736ebc3a5f14b2bb84a17c
alsu <9.alsulu@gmail.com>
2025-11-11 22:13:21 +0500

    update API access

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import jsonServer from 'json-server';
+import cors from 'cors';
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
